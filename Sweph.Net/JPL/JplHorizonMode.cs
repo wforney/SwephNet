bd65c474b9400698b2fc402d5408b991f580ae7f
Bill Forney <william@williamforney.com>
2025-06-28 04:56:09 -0700

    Add JplHorizonMode enum for JPL Horizons calculations
    Introduces a new namespace `Sweph.Net.JPL` and defines the `JplHorizonMode` enumeration. This enum includes three members: `None`, `JplHorizons`, and `JplApproximate`, each with descriptions and specific values. The changes improve the functionality for JPL Horizons calculations by providing distinct modes for varying levels of precision.

---
+++ Sweph.Net/JPL/JplHorizonMode.cs
@@ -0,0 +1,23 @@
+﻿namespace Sweph.Net.JPL;
+
+/// <summary>
+/// JPL Horizons calculation modes
+/// </summary>
+[Flags]
+public enum JplHorizonMode
+{
+    /// <summary>
+    /// None
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Reproduce JPL Horizons 1962 - today to 0.002 arcsec
+    /// </summary>
+    JplHorizons = 256 * 1024,
+
+    /// <summary>
+    /// Approximate JPL Horizons 1962 - today
+    /// </summary>
+    JplApproximate = 512 * 1024
+}
